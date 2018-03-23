11476402474ddd819cc44c6c40ad9b5667ea8ef8
Alwx <alwxsin@gmail.com>
2018-03-23 11:15:02 +0300

    we must use spaces

--- bot.py
+++ bot.py
@@ -13,26 +13,26 @@ GEO = 'geo'  # Georgia
 
 
 CONFIRMS = {
-	RUS: {
-		'confirm': 'Подтверждаю',
-		'hoot': 'Угу',
-		'shit': 'Хуйня'
-	},
-	ENG: {
-		'confirm': 'Confirm',
-		'hoot': 'Yea',
-		'shit': 'Bullshit'
-	},
-	GER: {
-		'confirm': 'Bekräftige',
-		'hoot': random.choice(['Ja', 'Ja-Ja', 'Jo', 'Stimmt']),
-		'shit': 'Dingsda'
-	},
-	GEO: {
-		'confirm': 'ვადასტურებ',
-		'hoot': 'ჰო',
-		'shit': 'ვადასტურებ'  # for now same as confirm
-	}
+    RUS: {
+        'confirm': 'Подтверждаю',
+        'hoot': 'Угу',
+        'shit': 'Хуйня'
+    },
+    ENG: {
+        'confirm': 'Confirm',
+        'hoot': 'Yea',
+        'shit': 'Bullshit'
+    },
+    GER: {
+        'confirm': 'Bekräftige',
+        'hoot': random.choice(['Ja', 'Ja-Ja', 'Jo', 'Stimmt']),
+        'shit': 'Dingsda'
+    },
+    GEO: {
+        'confirm': 'ვადასტურებ',
+        'hoot': 'ჰო',
+        'shit': 'ვადასტურებ'  # for now same as confirm
+    }
 }
 
 THUMB_URL = f'https://api.telegram.org/file/bot{TOKEN}/photos/file_2.jpg'
@@ -41,86 +41,86 @@ bot = telebot.TeleBot(TOKEN)
 
 
 def get_confirm_message(lang: str):
-	if lang not in CONFIRMS:
-		lang = RUS
-	r = random.random()
-	key = 'confirm' if r > 0.5 else 'hoot' if r > 0.1 else 'shit'
-	return CONFIRMS[lang][key]
+    if lang not in CONFIRMS:
+        lang = RUS
+    r = random.random()
+    key = 'confirm' if r > 0.5 else 'hoot' if r > 0.1 else 'shit'
+    return CONFIRMS[lang][key]
 
 
 def get_reply_payload(text: str, message_id: int) -> dict:
-	r = random.randint(1, 100)
+    r = random.randint(1, 100)
 
-	if 'филин' in text and 'подтверди' in text:
-		if r < 5:
-			return {'sticker': True}
-		return {
-			'text': get_confirm_message(RUS),
-			'reply_to_message_id': message_id
-		}
-	if 'owl' in text and 'confirm' in text:
-		return {
-			'text': get_confirm_message(ENG),
-			'reply_to_message_id': message_id
-		}
-	if 'der uhu' in text and 'bekräftig' in text:
-		return {
-			'text': get_confirm_message(GER),
-			'reply_to_message_id': message_id
-		}
-	if 'ზარნაშო' in text and 'დადასტურება' in text:
-		return {
-			'text': get_confirm_message(GEO),
-			'reply_to_message_id': message_id
-		}
-	if 'подтверди' in text:
-		if r < 5:
-			return {'sticker': True}
-		return {
-			'text': get_confirm_message(RUS),
-		}
-	if 'confirm' in text:
-		return {
-			'text': get_confirm_message(ENG),
-		}
-	if 'bekräftig' in text:
-		return {
-			'text': get_confirm_message(GER),
-		}
-	if 'დადასტურება' in text:
-		return {
-			'text': get_confirm_message(GER),
-		}
-	if 'сколк' in text:
-		return {
-			'text': 'Пидарасы',
-			'reply_to_message_id': message_id
-		}
+    if 'филин' in text and 'подтверди' in text:
+        if r < 5:
+            return {'sticker': True}
+        return {
+            'text': get_confirm_message(RUS),
+            'reply_to_message_id': message_id
+        }
+    if 'owl' in text and 'confirm' in text:
+        return {
+            'text': get_confirm_message(ENG),
+            'reply_to_message_id': message_id
+        }
+    if 'der uhu' in text and 'bekräftig' in text:
+        return {
+            'text': get_confirm_message(GER),
+            'reply_to_message_id': message_id
+        }
+    if 'ზარნაშო' in text and 'დადასტურება' in text:
+        return {
+            'text': get_confirm_message(GEO),
+            'reply_to_message_id': message_id
+        }
+    if 'подтверди' in text:
+        if r < 5:
+            return {'sticker': True}
+        return {
+            'text': get_confirm_message(RUS),
+        }
+    if 'confirm' in text:
+        return {
+            'text': get_confirm_message(ENG),
+        }
+    if 'bekräftig' in text:
+        return {
+            'text': get_confirm_message(GER),
+        }
+    if 'დადასტურება' in text:
+        return {
+            'text': get_confirm_message(GER),
+        }
+    if 'сколк' in text:
+        return {
+            'text': 'Пидарасы',
+            'reply_to_message_id': message_id
+        }
 
-	if r < 10:
-		return {
-			'text': CONFIRMS[RUS]['hoot']
-		}
-	return {}
+    if r < 10:
+        return {
+            'text': CONFIRMS[RUS]['hoot']
+        }
+    return {}
 
 
 @bot.edited_message_handler(content_types=['text'])
 @bot.message_handler(content_types=['text'])
 def text_handler(message: Message):
-	text = message.text.lower()
-	chat_id = message.chat.id
-	payload = get_reply_payload(text, message.message_id)
+    text = message.text.lower()
+    chat_id = message.chat.id
+    payload = get_reply_payload(text, message.message_id)
 
-	if 'sticker' in payload:
-		bot.send_sticker(chat_id, STIKER_ID)
-	elif payload:
-		bot.send_message(chat_id, **payload)
+    if 'sticker' in payload:
+        bot.send_sticker(chat_id, STIKER_ID)
+    elif payload:
+        bot.send_message(chat_id, **payload)
 
 
 @bot.inline_handler(lambda query: query.query)
 def query_text(inline_query):
-	r = InlineQueryResultArticle('1', 'Подтвердить', InputTextMessageContent('Подтверждаю'), thumb_url=THUMB_URL)
-	bot.answer_inline_query(inline_query.id, results=[r])
+    r = InlineQueryResultArticle('1', 'Подтвердить', InputTextMessageContent('Подтверждаю'), thumb_url=THUMB_URL)
+    bot.answer_inline_query(inline_query.id, results=[r])
 
 
 bot.polling()
